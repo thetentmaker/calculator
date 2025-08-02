33378c7b8de079d8b632a4723a51682e4d0ddb1c
tentmaker <ovsoc@daum.net>
2025-08-02 16:35:04 +0900

    feat: 계산기 기본 상태값 선언

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,21 @@
+import { useState } from 'react';
 import { SafeAreaView, Text } from 'react-native';
 
 export default function RootLayout() {
+  // 계산기
+  // 상태값 부터 구현
+  // input, setInput
+  // currentOperator, setCurrentOperator
+  // result, setResult
+  // tempInput, setTempInput
+  // tempOperator, setTempOperator
+  const [input, setInput] = useState('');
+  const [currentOperator, setCurrentOperator] = useState('');
+  const [result, setResult] = useState('');
+  const [tempInput, setTempInput] = useState('');
+  const [tempOperator, setTempOperator] = useState('');
+  
+
   return (
     <SafeAreaView>
       <Text>Hello</Text>
